f01c50b17276ef1d23f9f1c59cddab2c3a3a0861
sajclarke <shannonajclarke@gmail.com>
2022-01-02 16:57:46 -0400

    chore: update importing firebase env

--- utils/adminApp.ts
+++ utils/adminApp.ts
@@ -3,16 +3,16 @@ import * as firebaseAdmin from 'firebase-admin'
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
 // import serviceAccount from '../certs/barcodenetwork-secret.json'
+let serviceAccount
+if (typeof process.env.FIREBASE_SERVICE_ACCOUNT_KEY === 'string') {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+}
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-)
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(
-      serviceAccount.replace(/\\n/g, '\n')
-    ),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   })
 }
