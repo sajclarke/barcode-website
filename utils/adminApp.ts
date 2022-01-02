0bb4b9ccf70fbf066fa064e53178e39151d254e6
sajclarke <shannonajclarke@gmail.com>
2022-01-02 13:08:46 -0400

    chore: modify loading of firebase settings

--- utils/adminApp.ts
+++ utils/adminApp.ts
@@ -2,15 +2,15 @@ import * as firebaseAdmin from 'firebase-admin'
 
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
-import serviceAccount from '../certs/barcodenetwork-secret.json'
+// import serviceAccount from '../certs/barcodenetwork-secret.json'
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+)
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
-    }),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   })
 }
